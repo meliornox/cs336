ecb42e9f3cf329922a9bcc42170a21156507469b
meliornox <meliornox@gmail.com>
2016-10-07 23:50:03 -0400

    Update server.js

--- hw01/server.js
+++ hw01/server.js
@@ -1,8 +1,8 @@
 /**
- * Sample server for CS 336, Unit 4
+ * Server for CS 336, Homework 01
  *
- * @author kvlinden
- * @version summer2016
+ * @author meliornox, Jay Bigelow
+ * @version 10072016
  */
 
 const express = require('express')
@@ -10,51 +10,55 @@ const app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
-class Person {
-    //startDate is a Date object
-    constructor(firstName, lastName, loginID, startDate) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.loginID = loginID;
-        this.startDate = startDate;
-    }
-    seniority() {
-        return 2016 - this.startDate.getFullYear();
-    }
-    name() {
-        return this.firstName + " " + this.lastName;
-    }
-}
-
-var employees = {
-  "amb23": { firstName : "Alice", lastName : "Barnes", startDate : "08/04/2012" },
-  "als09": { firstName : "Anna", lastName : "Smith", startDate : "05/09/2014"}
-};
-
 app.listen(app.get('port'), function() {
     console.log('Listening on port ' + app.get('port') + '...');
 });
 
+//List of employees
+var employees = {
+  "amb23": { firstName : "Alice", lastName : "Barnes", startDate : "2012/08/04" },
+  "als09": { firstName : "Anna", lastName : "Smith", startDate : "2014/05/09"}
+};
+
+//root response
 app.get('/', function (req,res) {
     res.send('Hello World!');
 });
 
+//lists people
 app.get('/people', function (req,res) {
     res.json(employees);
 });
 
+//gives info of person loginID, if none exists then gives a 404
 app.get('/person/:loginID', function(req, res) {
-    res.send(employees[req.params.loginID]);
+	if (employees[req.params.loginID] != null){
+		res.json(employees[req.params.loginID]);
+	} else{
+		res.sendStatus(404);
+	}
 });
 
-//todo
-app.get('/person/:userID/name', function(req, res) {
-  var person = employees[req.params.loginID];
-    res.send(person.firstName());
+//gives name of person loginID, if none exists then gives a 404
+app.get('/person/:loginID/name', function(req, res) {
+	if (employees[req.params.loginID] != null){
+		res.json(employees[req.params.loginID].firstName + " " + 
+		employees[req.params.loginID].lastName);
+	} else{
+		res.sendStatus(404);
+	}
 });
 
-//todo
-app.get('/person/:userID/year', function(req, res) {
-    res.send('Year?');
-    
+//gives seniority of person loginID, if none exists then gives a 404
+app.get('/person/:loginID/year', function(req, res) {
+	if (employees[req.params.loginID] != null){
+		var startDate = new Date(employees[req.params.loginID].startDate);
+	
+		var date = new Date();
+		var time = date.getFullYear() - startDate.getFullYear();
+
+		res.json(time + " years");
+	} else{
+		res.sendStatus(404);
+	}
 });
