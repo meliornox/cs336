4929e8b974d6c13f229f47026c062a6f2ef588ae
Jay Bigelow <jgb23@sutherland.cs.calvin.edu>
2016-10-28 17:15:44 -0400

    Finished lab08

---
+++ lab07/public/jquery/script-jquery.js
@@ -0,0 +1,11 @@
+
+"use strict";
+
+$(document).ready(function() {
+    $("#faqs h2").click(
+        function() {
+            $(this).next("div").toggleClass("open");
+            $(this).toggleClass("minus");
+        }
+    );
+});
