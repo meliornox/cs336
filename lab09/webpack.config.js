30c93411e4633568c12f85c2c617f071d59419fb
Jay Bigelow <jgb23@sutherland.cs.calvin.edu>
2016-11-04 13:39:25 -0400

    Finished 9.3

--- lab09/webpack.config.js
+++ lab09/webpack.config.js
@@ -1,3 +1,4 @@
+var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
@@ -16,5 +17,14 @@ module.exports = {
     },
     plugins: [
 	new HtmlWebpackPlugin({template: __dirname + "/app/index.tmpl.html"})
-    ]
+	new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        port: 3001,
+        proxy: { '/api/*': 'http://localhost:3000' },
+        colors: true,
+        historyApiFallback: true,
+        inline: true,
+        hot: true
+    }
 };
