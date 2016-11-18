34f9ef70a5f1abc5666bc10164ec25f14d410b2e
meliornox <meliornox@gmail.com>
2016-11-18 17:34:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -14,8 +14,15 @@ var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient
+var MongoClient = require('mongodb').MongoClient;
 var app = express();
+
+var db;
+// var mongoPassword = bjarne;
+MongoClient.connect('mongodb://cs336:' + process.env.MONGO_PASSWORD + '@ds029297.mlab.com:29297/cs336', function (err, dbConnection) {
+  if (err) throw err;
+  db = dbConnection;
+});
 
 var COMMENTS_FILE = path.join(__dirname, 'comments.json');
 
@@ -25,48 +32,27 @@ app.use('/', express.static(path.join(__dirname, 'dist')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Additional middleware which will set headers that we need on each request.
 app.use(function(req, res, next) {
-    // Set permissive CORS header - this allows this server to be used only as
-    // an API server in conjunction with something like webpack-dev-server.
     res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Disable caching so we'll always get the latest comments.
     res.setHeader('Cache-Control', 'no-cache');
     next();
 });
 
 app.get('/api/comments', function(req, res) {
-    db.collection('comments').find({}).toArray(function(err, docs) {
-	if (err) throw err;
-	res.json(docs);
-    });
+  db.collection("comments").find({}).toArray(function (err, data) {
+    if (err) throw error;
+    res.json(data);
+  });
 });
 
 app.post('/api/comments', function(req, res) {
-    var newComment = {
-        id: Date.now(),
-        author: req.body.author,
-        text: req.body.text,
-    };
-    db.collection('comments').insertOne(newComment, function(err, result) {
-	if (err) throw err;
-	db.collection('comments').find({}).toArray(function(err, docs) {
-	    if (err) throw err;
-	    res.json(docs);
-	});
-    });
+  db.collection("comments").insert({
+    id: Date.now(),
+    author: req.body.author,
+    text: req.body.text
+  });
 });
 
-var db;
-var PASSWORD = process.env.MONGO_PASSWORD;
-var mongoURL = 'mongodb://cs336:' + PASSWORD + '@ds139567.mlab.com:39567/cs336';
-
-MongoClient.connect(mongoURL, function (err, dbConnection) {
-  if (err) throw err
-  db = dbConnection;
-})
-
 app.listen(app.get('port'), function() {
-    console.log('Server started: http://localhost:' + app.get('port') + '/');
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
