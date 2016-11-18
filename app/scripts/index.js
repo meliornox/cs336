92c718cb34d51d7395f530493a73b1b927ca59f8
meliornox <meliornox@gmail.com>
2016-11-18 17:18:00 -0500

    Update index.js

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -16,6 +16,8 @@ import ReactDOM from 'react-dom';
 
 import CommentBox from './commentBox';
 
+import '../css/base.css';
+
 ReactDOM.render(
     <CommentBox url="/api/comments" pollInterval={2000}/>,
     document.getElementById('content')
